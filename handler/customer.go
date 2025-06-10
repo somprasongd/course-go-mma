ddcfc7e8f775bd20689b05904ce15084ce11d2c4
Somprasong Damyos <somprasong.damyos@gmail.com>
2025-06-10 14:50:37 +0700

    014-02-custom-error: using response.JSONError(...)

--- handler/customer.go
+++ handler/customer.go
@@ -6,6 +6,7 @@ import (
 	"go-mma/service"
 	"go-mma/util/errs"
 	"go-mma/util/logger"
+	"go-mma/util/response"
 
 	"github.com/gofiber/fiber/v3"
 )
@@ -24,21 +25,16 @@ func (h *CustomerHandler) CreateCustomer(c fiber.Ctx) error {
 	// แปลง request body -> dto
 	var req dto.CreateCustomerRequest
 	if err := c.Bind().Body(&req); err != nil {
-		// แปลงไม่ได้ให้ส่ง error 400
-		errResp := errs.InputValidationError(err.Error()) // <-- ปรับมาเป็น AppError
-		return c.Status(
-			errs.GetHTTPStatus(errResp), // <-- ดึง status code จาก AppError
-		).JSON(errResp)
+		// <-- เปลี่ยนมาใช้ reponse จัดการ Error
+		return response.JSONError(c, errs.InputValidationError(err.Error()))
 	}
 
 	logger.Log.Info(fmt.Sprintf("Received customer: %v", req))
 
 	// ตรวจสอบ input fields (e.g., value, format, etc.)
 	if err := req.Validate(); err != nil {
-		errResp := errs.InputValidationError(err.Error()) // <-- ปรับมาเป็น AppError
-		return c.Status(
-			errs.GetHTTPStatus(errResp), // <-- ดึง status code จาก AppError
-		).JSON(errResp)
+		// <-- เปลี่ยนมาใช้ reponse จัดการ Error
+		return response.JSONError(c, errs.InputValidationError(err.Error()))
 	}
 
 	// ส่งไปที่ Service Layer
@@ -46,9 +42,8 @@ func (h *CustomerHandler) CreateCustomer(c fiber.Ctx) error {
 
 	// จัดการ error จาก Service Layer หากเกิดขึ้น
 	if err != nil {
-		return c.Status(
-			errs.GetHTTPStatus(err), // <-- ดึง status code จาก AppError
-		).JSON(fiber.Map{"error": err.Error()})
+		// <-- เปลี่ยนมาใช้ reponse จัดการ Error
+		return response.JSONError(c, err)
 	}
 
 	// ตอบกลับด้วย status code 201 (created) และข้อมูลแบบ JSON
